3ad4aa573bd0f58b9b4069ab4c25f1521df70233
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-08 17:42:45 -0800

    further commit 2 JS drink

--- 04/#2 JSDrinkList solved/oakes.js
+++ 04/#2 JSDrinkList solved/oakes.js
@@ -34,8 +34,54 @@ for (var j = 1; j < drinkList.length; j++) {
     targetDiv.appendChild(newDiv);
 
 }
+///SOLVED TRILOGY///////
+/*// This line of JavaScript "grabs" the main div on the page ("#drink-options");
+    var drinkDiv = document.getElementById("drink-options");
 
-// ...
+    // We then use a for loop to iterate through all the drinks in drinkList.
+    // With each iteration, we perform a series of code operations each time.
+    for (var i = 0; i < drinkList.length; i++) {
+
+      // For each drink in the array, we create a new paragraph to hold that text.
+      // A new paragraph will be created with each iteration of the loop.
+      var newDrinkP = document.createElement("p");
+
+      // We then assign the the text of this paragraph to be the text in the array.
+      newDrinkP.textContent = drinkList[i];
+
+      // We then add the paragraph to the our main div on the page ("#drink-options")
+      drinkDiv.appendChild(newDrinkP);
+    }*/
 
 
+/////SOLVED TJ/////
+/*   {name: 'Coffee', value: 5},
+   {name: 'Espresso', value: 7},
+   {name: 'Cappuccino', value: 6},
+   {name: 'Latte', value: 4},
+   {name: 'Tea', value: 3}
+ ];
+
+ // 1. Create code that "grabs" the div with the matching id (#drink-options);
+ // ...
+ // ...
+ var drinksContainer = document.querySelector('#drink-options');
+
+ // 2. Create a for loop that creates HTML content of all the drinks using JavaScript.
+ // HINT: You will need to use each of the following methods: createElement, textContent, appendChild
+ // ...
+ // ...
+ drinkList.forEach(function(val, index, arr) {
+   var newDiv = document.createElement('div');
+   newDiv.setAttribute('class', 'item');
+   var nameSpan = document.createElement('span');
+   var priceSpan = document.createElement('span');
+   nameSpan.textContent = val.name + ": ";
+   nameSpan.setAttribute('class', 'itemName');
+   priceSpan.setAttribute('class', 'itemValue');
+   priceSpan.textContent = '$' + val.value;
+   newDiv.appendChild(nameSpan);
+   newDiv.appendChild(priceSpan);
+   drinksContainer.appendChild(newDiv);
+ })*/
 
