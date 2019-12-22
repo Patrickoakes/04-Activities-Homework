0aab69b44f91420999702e3269e41ac2effe66ae
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-21 21:39:11 -0800

    commit crash course

--- 04/#21 Crash Course/solved.js
+++ 04/#21 Crash Course/solved.js
@@ -5,54 +5,43 @@
 // ==========================================================
 
 // Create a basic variable
-
-var num = 5;
-////console.log((num = 7));
+var name = "Ahmed";
 
 // 2. ARRAYS
 // ==========================================================
 
 // Create an array of five strings
-
-var fingers = ["pinky", "ring", "middle", "thumb", "index"];
-//console.log(fingers[1]);
-
-// Create an array of five numbers
-
-var num = [0, 1, 2, 3, 4];
-//console.log(num[3]);
+var fiveArray = ["Ahmed", "Lexus", "Tom", "Jim", "Joy"];
 
 // 3. FOR LOOPS
 // ==========================================================
 
 // Create a for loop that loops through and prints "My name is Bob five times"
-
-var arr = ["My name ", "is ", "Bob ", "five ", "times"];
-
-for (i = 0; i < arr; i++) {
-  console.log(arr[i]);
+for (var i = 0; i < 5; i++) {
+  console.log("My name is Bob");
 }
-console.log("My name is " + arr[2] + "five times");
-// Create a for loop that loops through your five string array
-for (i = 0; i < arr; i++) {
-  console.log(arr[i]);
+
+// Create a for loop that loops through your five string array and prints each name
+for (var i = 0; i < fiveArray.length; i++) {
+  console.log("My name is " + fiveArray[i]);
 }
-console.log(arr);
+
 // 4. FUNCTIONS
 // ==========================================================
 
 // Create a function that takes two numbers and divides the first number by the second.
 // Then call that function
-function numDiv(dividend, divisor) {
-  quotient = dividend / divisor;
+var divider = function(num1, num2) {
+  var result = num1 / num2;
+  console.log(result);
+  return result;
+};
 
-  return quotient;
-}
-console.log(numDiv(4, 2));
-// Create a function that takes in an array of numbers and then loops through the array
-// and prints out numbers.
+// Prints 5
+divider(10, 2);
+
+// Create a function that takes in an array of strings and then loops through the array and prints out strings.
 // Then call that function
-var fiveArray = ["Ahmed", "Lexus", "Tom", "Jim", "Joy"];
 var looper = function(arrayNumbers) {
   for (var i = 0; i < arrayNumbers.length; i++) {
     console.log(arrayNumbers[i]);
@@ -61,50 +50,10 @@ var looper = function(arrayNumbers) {
 
 looper(fiveArray);
 
-/*function arr(i, num) {
-  var num = [0, 1, 2, 3, 4];
-  for (i = 0; i < num.length; i++);
-
-  return num[i];
-}
-console.log(arr(0, [i]));
-
-/*function howMany(selectObject) {
-  var numberSelected = 0;
-  var loop = [0, 1, 2, 3, 4];
-  for (var i = 0; i < loop.length; i++) {
-    if (selectObject.loop[i]) {
-      numberSelected++;
-    }
-  }
-  return numberSelected;
-}
-console.log(howMany(5, 1));
-
 // 5. OBJECTS
 // ==========================================================
 
 // Create a JavaScript Object
-
-/*var person = new Object();
-
-
-
-var person = {};
-
-var person = {
-    firstname = "John",
-    lastname = "Smith",
-    age = 27
-};
-console.log(person.age);
-
-// Console log any three of the properties in that object
-
-// Create an Array of 5 Objects
-
-// Console log 3 properties for every one of the five objects*/
-
 var monsterTruck = {
   name: "Digital Destructor",
   brand: "Honda",
@@ -146,12 +95,7 @@ for (var i = 0; i < threeObjects; i++) {
 // ==========================================================
 
 // Create a basic html button then create an onClick event that triggers an on click event.
-
-$(document).ready(function() {
-  $("#button").click(function() {
-    $("#demo").append("<p> Hello World </p>");
-  });
-});
+// Do this on a new page or see previous example
 
 // 7. OVERALL STRUCTURE
 // ==========================================================
